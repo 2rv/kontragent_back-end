323c44ca0865a630d902c3f966db3df3948e2e35
David Harutyunyan <54120867+harutyunaraci@users.noreply.github.com>
2021-11-24 17:52:39 +0300

    edited company-member-add controller

--- src/core/company-member/company-member.service.ts
+++ src/core/company-member/company-member.service.ts
@@ -6,8 +6,9 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { CompanyEntity } from '../company/company.entity';
 import { USER_ROLE } from '../user/enum/user-role.enum';
-import { UserEntity } from '../user/user.entity';
+import { USER_ERROR } from '../user/enum/user-error.enum';
 import { CompanyMemberRepository } from './company-member.repository';
+import { UserRepository } from '../user/user.repository';
 import { CompanyMemberEntity } from './company-memeber.entity';
 import { GetcompanyMemberListDto } from './dto/get-company-user-list.dto';
 import { COMPANY_MEMBER_ERROR } from './enum/company-member-error.enum';
@@ -18,13 +19,31 @@ export class CompanyMemberService {
   constructor(
     @InjectRepository(CompanyMemberRepository)
     private companyMemberRepository: CompanyMemberRepository,
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
   ) {}
 
   async createCompanyMember(
     company: CompanyEntity,
-    user: UserEntity,
+    userId: string,
     role: COMPANY_MEMBER_ROLE,
   ): Promise<void> {
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+    });
+
+    if (String(user.role) === String(USER_ROLE.BLOCKED)) {
+      throw new BadRequestException(USER_ERROR.USER_IS_BLOKED);
+    }
+    if (!user) {
+      throw new BadRequestException(USER_ERROR.CANNOT_FIND_USER);
+    }
+    if (user.role === USER_ROLE.ADMIN) {
+      throw new BadRequestException(
+        COMPANY_MEMBER_ERROR.CANNOT_ADD_SUCH_A_USER,
+      );
+    }
+
     const companyMember = await this.companyMemberRepository
       .createQueryBuilder('company-member')
       .leftJoin('company-member.company', 'company')
@@ -36,13 +55,6 @@ export class CompanyMemberService {
     if (companyMember) {
       throw new BadRequestException(
         COMPANY_MEMBER_ERROR.USER_ALREADY_HAS_COMPANY_ACCOUNT,
-      );
-    }
-
-    if(user.role === USER_ROLE.ADMIN)
-    {
-      throw new BadRequestException(
-        COMPANY_MEMBER_ERROR.CANNOT_ADD_SUCH_A_USER,
       );
     }
 
