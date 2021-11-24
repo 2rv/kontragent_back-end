e9a5d8b285fabe684c2e0bc6d8ade0039e41a18d
David Harutyunyan <54120867+harutyunaraci@users.noreply.github.com>
2021-11-24 17:16:30 +0300

    removed .craete cors option

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,9 @@ import { API_SERVER_PORT } from './config/server.config';
 export async function bootstrap() {
   const port = process.env.PORT || API_SERVER_PORT;
 
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: true });
+  app.enableCors();
   app.use(helmet());
   await app.listen(port);
 
