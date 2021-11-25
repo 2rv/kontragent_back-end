06f94b39ea2eea9856fee30c7d2c5672989187bd
David Harutyunyan <54120867+harutyunaraci@users.noreply.github.com>
2021-11-25 12:17:19 +0300

    added enableCors configuration

--- src/main.ts
+++ src/main.ts
@@ -10,17 +10,14 @@ export async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PUTCH');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-
-  app.enableCors({
-    allowedHeaders: '*',
+  const options = {
     origin: '*',
-  });
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  app.enableCors(options);
 
   app.use(helmet());
   await app.listen(port);
