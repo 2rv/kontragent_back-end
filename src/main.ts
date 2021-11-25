00ab217cb0df186db2f62999fe25b225badf230d
David Harutyunyan <54120867+harutyunaraci@users.noreply.github.com>
2021-11-25 12:36:05 +0300

    back to defaults

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,9 @@ import { API_SERVER_PORT } from './config/server.config';
 export async function bootstrap() {
   const port = process.env.PORT || API_SERVER_PORT;
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
-  const options = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  };
-  app.enableCors(options);
+  app.enableCors();
 
   app.use(helmet());
   await app.listen(port);
