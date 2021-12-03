edc1e46a6a57df943eeda7481df5a5be532e8dad
Mi4-04 <zakharovm2001@mail.ru>
2021-12-03 13:18:52 +0300

    add companyMemberGuard

--- src/core/bill/bill.controller.ts
+++ src/core/bill/bill.controller.ts
@@ -1,11 +1,11 @@
 import {
-    Body,
-    Controller,
-    Post,
-    Patch,
-    Get,
-    UseGuards,
-    ValidationPipe,
+  Body,
+  Controller,
+  Post,
+  Patch,
+  Get,
+  UseGuards,
+  ValidationPipe,
 } from '@nestjs/common';
 import { AccountGuard } from '../user/guard/account.guard';
 import { AuthGuard } from '@nestjs/passport';
@@ -15,7 +15,7 @@ import { GetAccount } from '../user/decorator/get-account.decorator';
 import { GetBill } from './decorator/get-bill.decorator';
 import { BillGuard } from './guard/bill.guard';
 import { UserEntity } from '../user/user.entity';
-import {BillService} from './bill.service'
+import { BillService } from './bill.service';
 import { BillEntity } from './bill.entity';
 import { CompanyGuard } from '../company/guard/company.guard';
 import { BillCompanyCompanyIdDto } from './dto/bill-company-companyId.dto';
@@ -25,23 +25,24 @@ import { BILL_STATUS } from './enum/bill-status.enum';
 import { UpdateBillDto } from './dto/update-bill.dto';
 import { CompanyBalanceService } from '../company-balance/company-balance.service';
 import { GetBillListDto } from './dto/get-bill-list.dto';
+import { CompanyMemberGuard } from '../company-member/guard/company-member.guard';
 
 @Controller('bill')
 export class BillController {
   constructor(private billService: BillService) {}
 
   @Post('/company/:companyId/')
-  @UseGuards(AuthGuard(), AccountGuard, CompanyGuard)
+  @UseGuards(AuthGuard(), AccountGuard, CompanyMemberGuard, CompanyGuard)
   createCompanyBill(
-      @Body(ValidationPipe) billCompanyCompanyIdDto: BillCompanyCompanyIdDto,
-      @GetAccount() user: UserEntity,
-      @GetCompany() company: CompanyEntity,
+    @Body(ValidationPipe) billCompanyCompanyIdDto: BillCompanyCompanyIdDto,
+    @GetAccount() user: UserEntity,
+    @GetCompany() company: CompanyEntity,
   ): Promise<void> {
-   return this.billService.createCompanyBill(
-    billCompanyCompanyIdDto,
-    company,
-    user,
-   )  
+    return this.billService.createCompanyBill(
+      billCompanyCompanyIdDto,
+      company,
+      user,
+    );
   }
 
   @Patch('/:billId')
@@ -49,27 +50,22 @@ export class BillController {
   @UseGuards(AuthGuard(), AccountGuard, BillGuard)
   fillCompanyBill(
     @Body(ValidationPipe) updateBillDto: UpdateBillDto,
-    @GetBill() bill: BillEntity
+    @GetBill() bill: BillEntity,
   ): Promise<void> {
-    return this.billService.fillCompanyBill(
-        updateBillDto,
-        bill,
-    )
+    return this.billService.fillCompanyBill(updateBillDto, bill);
   }
 
   @Patch('/fulfill/:billId')
   @Roles(USER_ROLE.ADMIN)
   @UseGuards(AuthGuard(), AccountGuard, BillGuard)
-  fulfillCompanyBill(
-    @GetBill() bill: BillEntity,
-  ): Promise<void> {
-    return this.billService.fulfillCompanyBill(bill) 
+  fulfillCompanyBill(@GetBill() bill: BillEntity): Promise<void> {
+    return this.billService.fulfillCompanyBill(bill);
   }
 
   @Get('/bill-list')
   @Roles(USER_ROLE.ADMIN)
   @UseGuards(AuthGuard(), AccountGuard)
   getAdminBillList(): Promise<GetBillListDto> {
-      return this.billService.getAdminBillList()
+    return this.billService.getAdminBillList();
   }
 }
