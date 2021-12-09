76b01b1bd26e441ba4b5acff98dee231bee568de
David Harutyunyan <54120867+harutyunaraci@users.noreply.github.com>
2021-12-09 22:45:43 +0300

    client sms service

--- src/common/utils/sms.ts
+++ src/common/utils/sms.ts
@@ -1,7 +1,7 @@
 import fetch from 'cross-fetch';
 
 export async function sendCodeSMS(message: string, to: string) {
-  const url = `https://arutyunyan.davitt@yandex.ru:dHx02iTCaBOEqLfYhhEW7SyyZvkM@gate.smsaero.ru/v2/sms/send?number=${to}&text=${message}&sign=SMS Aero`;
+  const url = `https://igroovei@inbox.ru:nJ7e2RuOtIoXT620chuCxyBBbbJ7@gate.smsaero.ru/v2/sms/send?number=${to}&text=${message}&sign=Kontragent`;
   const encodedURI = encodeURI(url);
 
   const response = await fetch(encodedURI);
